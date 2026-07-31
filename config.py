35e71be6072725d932128b5d6ca8f0ec8ddddf3c
humanRightsAggregator <human.rights.aggregator@gmail.com>
2026-07-31 23:53:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,13 @@ TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
 SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
+
+# Meta APIs
 META_ACCESS_TOKEN = os.environ.get("META_ACCESS_TOKEN", "")
 THREADS_USER_ID = os.environ.get("THREADS_USER_ID", "")
+FB_PAGE_ID = os.environ.get("FB_PAGE_ID", "")
+FB_PAGE_ACCESS_TOKEN = os.environ.get("FB_PAGE_ACCESS_TOKEN", "")
+INSTAGRAM_ACCOUNT_ID = os.environ.get("INSTAGRAM_ACCOUNT_ID", "")
 
 # News Feed Sources
 RSS_FEEDS = [
